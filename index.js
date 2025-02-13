e58eca11d5ff0b8cb394eabbb47ccd86acc61753
Adhishvar27 <adhishvar.46@gmail.com>
2025-02-13 18:27:48 +0530

    Simple HTML program

---
+++ index.js
@@ -0,0 +1,13 @@
+// Write your code below:
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const mainHeading = document.getElementById('main-heading');
+mainHeading.style.color = 'orange';
+mainHeading.textContent = 'Fruit World';
+
+const basketheading = document.getElementById('basket-heading');
+basketheading.style.color = 'green';
+const thanks = document.getElementById('thanks');
+thanks.innerHTML ='<p>Please visit us again</p>'
